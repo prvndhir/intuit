07f2d04944c8529c6a12d02837b645248b684558
prdhir <pravndhir@gmail.com>
2017-11-26 13:21:02 -0800

    set catalina home

--- scripts/tomcat/tomcat.sh
+++ scripts/tomcat/tomcat.sh
@@ -5,7 +5,7 @@ set -x
         . ${HOME}/intuit/scripts/tomcat/setenv.sh
         echo "JAVA_HOME=$JAVA_HOME"
         echo "CATALINA_HOME=$CATALINA_HOME"
-        exho "PATH=$PATH"
+        echo "PATH=$PATH"
         echo "CATALINA_OPTS=$CATALINA_OPTS"
 #fi
 if [ "${1}" = "start" ]; then
