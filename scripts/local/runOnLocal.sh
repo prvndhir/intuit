1fa18af4506d03bc0371ff6703068398d34b94a4
prdhir <pravndhir@gmail.com>
2017-11-26 15:53:06 -0800

    Modified ip and copying config from one available in repo

--- scripts/local/runOnLocal.sh
+++ scripts/local/runOnLocal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws_ip="34.212.123.141"
+aws_ip="34.215.231.40"
 #aws_ip="${1}"
 archive="archive"
 archive_src="${HOME}/archive"
@@ -10,8 +10,10 @@ cd ${HOME}
 tar -cvf ${archive_tar} ${archive}
 scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/keys ubuntu@${aws_ip}:${ubuntu_home}
 scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${archive_tar} ubuntu@${aws_ip}:${ubuntu_home}
-scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/.ssh/config ubuntu@${aws_ip}:${ubuntu_home}.ssh/
-ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "cd ${ubuntu_home};git clone git@github.com:prvndhir/intuit.git;bash intuit/scripts/remote/runOnPlainUbuntu.sh"
+scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/intuit/conf/ssh/config ubuntu@${aws_ip}:${ubuntu_home}/.ssh/
+ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "cd /home/ubuntu;git clone git@github.com:prvndhir/intuit.git;bash intuit/scripts/remote/runOnPlainUbuntu.sh"
 #ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "tar -xvf ${archive_dest}/${archive}.tar"
-#ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@34.212.123.141
+#ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@34.215.231.40
 #cd ${HOME};git clone git@github.com:prvndhir/intuit.git;cd intuit
+
+
