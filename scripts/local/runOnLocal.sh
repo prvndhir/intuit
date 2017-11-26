67fafa53417f6391fde1f698c77f0ddda1ea5776
prdhir <pravndhir@gmail.com>
2017-11-26 14:17:43 -0800

    executing remote from local

--- scripts/local/runOnLocal.sh
+++ scripts/local/runOnLocal.sh
@@ -4,15 +4,14 @@ aws_ip="34.212.123.141"
 archive="archive"
 archive_src="${HOME}/archive"
 archive_tar="/tmp/${archive}.tar"
-archive_dest="/home/ubuntu/"
+ubuntu_home="/home/ubuntu"
 rm -rf ${archive_tar}
 cd ${HOME}
 tar -cvf ${archive_tar} ${archive}
-scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/keys ubuntu@${aws_ip}:/home/ubuntu/
-scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${archive_tar} ubuntu@${aws_ip}:${archive_dest}
-scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/.ssh/config ubuntu@${aws_ip}:/home/ubuntu/.ssh/
-#aws_ip="34.212.123.141";scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/keys ubuntu@${aws_ip}:/home/ubuntu/
-#aws_ip="34.212.123.141";scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/.ssh/config ubuntu@${aws_ip}:/home/ubuntu/.ssh/
+scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/keys ubuntu@${aws_ip}:${ubuntu_home}
+scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${archive_tar} ubuntu@${aws_ip}:${ubuntu_home}
+scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/.ssh/config ubuntu@${aws_ip}:${ubuntu_home}.ssh/
+ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "cd ${ubuntu_home};git clone git@github.com:prvndhir/intuit.git;bash intuit/scripts/remote/runOnPlainUbuntu.sh"
 #ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "tar -xvf ${archive_dest}/${archive}.tar"
 #ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@34.212.123.141
 #cd ${HOME};git clone git@github.com:prvndhir/intuit.git;cd intuit
