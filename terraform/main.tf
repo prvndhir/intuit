1ba186d73c8ef0e1f6ad68c0522bd898b38c5bdb
prdhir <pravndhir@gmail.com>
2017-11-26 15:53:43 -0800

    Simplified terraform template

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,41 +5,15 @@ provider "aws" {
 resource "aws_eip_association" "eip_assoc" {
   instance_id = "${aws_instance.docker-standalone.id}"
   allocation_id = "${aws_eip.docker-standalone-ip.id}"
-
-  connection {
-    host = "${aws_eip.docker-standalone-ip.public_ip}"
-    user = "ubuntu"
-  }
-
-  provisioner "file" {
-    destination = "~/.ssh/config"
-    content = <<EOF
-Host github.com
-StrictHostKeyChecking no
-UserKnownHostsFile /dev/null
-EOF
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get -y install git",
-      "curl -L get.docker.io | sudo sh",
-      "sudo usermod -aG docker ubuntu",
-      "sudo /bin/sh -c 'curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose'"
-    ]
-
-  }
-
 }
 
 resource "aws_instance" "docker-standalone" {
   count = 1
   key_name = "${var.aws_keypair}"
   tags {
-    Name = "Farallones-Docker-Orchestration"
-    Type = "terraform-test"
-    finance = "NSN_RnD"
+    Name = "myintuit-app"
+    Type = "intuit-tomcat"
+    finance = "self"
     Owner = "${var.aws_keypair}"
   }
   instance_type = "t2.micro"
