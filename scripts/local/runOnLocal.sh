b698a5062483bfadb0c388a320bd5f5fb8bd807a
prdhir <pravndhir@gmail.com>
2017-12-03 22:55:41 -0800

    added terraform to local script

--- scripts/local/runOnLocal.sh
+++ scripts/local/runOnLocal.sh
@@ -18,6 +18,7 @@ scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/k
 scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${archive_tar} ubuntu@${aws_ip}:${ubuntu_home}
 scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/intuit/conf/ssh/config ubuntu@${aws_ip}:${ubuntu_home}/.ssh/
 ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "cd /home/ubuntu;git clone git@github.com:prvndhir/intuit.git;bash intuit/scripts/remote/runOnPlainUbuntu.sh"
+echo "Please try accessing sample app at http://${aws_ip}"
 #ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "tar -xvf ${archive_dest}/${archive}.tar"
 #ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@34.212.53.1
 #cd ${HOME};git clone git@github.com:prvndhir/intuit.git;cd intuit
