5694cce47da38bd1952228d9c72c4e7fc0427458
pdhir <pdhir@sensity.com>
2017-12-06 19:19:47 -0800

    Expecting it to run from work dir

--- scripts/local/runOnLocal.sh
+++ scripts/local/runOnLocal.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
-terraform_dir="${HOME}/intuit/terraform"
+WORK_DIR=${1}
+cd ${WORK_DIR}
+terraform_dir="${WORK_DIR}/terraform"
 cp ${HOME}/keys/credentials ${HOME}/.aws/
 cd ${terraform_dir}
-terraform init -input=false
-terraform plan -out=tfplan -input=false
-terraform apply -input=false tfplan
+/usr/local/bin/terraform init -input=false
+/usr/local/bin/terraform plan -out=tfplan -input=false
+/usr/local/bin/terraform
+ apply -input=false tfplan
 aws_ip="$(grep "value" ${terraform_dir}/terraform.tfstate| sed 's/"value": "//g'|sed 's/"//g'| sed 's/ //g')"
 echo "aws_ip=${aws_ip}"
 #aws_ip="${1}"
-archive="archive"
-archive_src="${HOME}/archive"
-archive_tar="/tmp/${archive}.tar"
 ubuntu_home="/home/ubuntu"
-rm -rf ${archive_tar}
-cd ${HOME}
+cd ${WORK_DIR}
 #tar -cvf ${archive_tar} ${archive}
 scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/keys ubuntu@${aws_ip}:${ubuntu_home}
 #scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${archive_tar} ubuntu@${aws_ip}:${ubuntu_home}
-scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${HOME}/intuit/conf/ssh/config ubuntu@${aws_ip}:${ubuntu_home}/.ssh/
+scp -r -o StrictHostKeyChecking=no -i ${HOME}/keys/temp_key_my_aws.pem ${WORK_DIR}/conf/ssh/config ubuntu@${aws_ip}:${ubuntu_home}/.ssh/
 ssh -i ${HOME}/keys/temp_key_my_aws.pem ubuntu@${aws_ip} "cd /home/ubuntu;git clone git@github.com:prvndhir/intuit.git;bash intuit/scripts/remote/runOnPlainUbuntu.sh ${aws_ip}"
 t=`echo ${aws_ip} |sed 's/\./-/g'`
 hostdns="ec2-$t.us-west-2.compute.amazonaws.com"
